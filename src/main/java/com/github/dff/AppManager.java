513570857c684fac28a79cc4f8044837f12aca04
Gonçalo Prendi <goncalo.prendi25@gmail.com>
2017-01-03 23:13:02 +0000

    Initial class creation

---
+++ src/main/java/com/github/dff/AppManager.java
@@ -0,0 +1,14 @@
+package com.github.dff;
+
+/**
+ * <Add documentation here>
+ *
+ * @author Gonçalo Prendi (goncalo.prendi25@gmail.com)
+ * @since 1.0.0
+ */
+public class AppManager {
+
+    public static void main(String[] args) {
+        
+    }
+}
